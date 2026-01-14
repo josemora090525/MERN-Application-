56e290a1fc93069b725b3d3746105bf8481b2560
josemora09052025 <josedavid.morap@gmail.com>
2026-01-13 20:41:15 -0500

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from "path";
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
@@ -17,6 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const __dirname = path.resolve();
+
 app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5175",
     credentials: true
@@ -30,6 +33,14 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
